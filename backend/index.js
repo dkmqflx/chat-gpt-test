bd2a32e15abbf2f0dab0514befd3793c7fe9d5ae
dkmqflx <dkmqflx@gmail.com>
2024-05-23 12:34:44 +0900

    섹션4: 기능 고도화 하기 - 14. 여러 건의 채팅 메세지 연결하기

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-const apiKey = "sk-";
+const apiKey = "";
 const { Configuration, OpenAIApi } = require("openai");
 const express = require("express");
 var cors = require("cors");
@@ -22,29 +22,56 @@ app.use(express.urlencoded({ extended: true })); // for parsing application/x-ww
 
 // POST method route
 app.post("/fortuneTell", async function (req, res) {
+  let { userMessages, assistantMessages } = req.body;
+
+  let messages = [
+    {
+      role: "system",
+      content:
+        "당신은 세계 최고의 점성술사입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 챗도지입니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다.",
+    },
+    {
+      role: "user",
+      content:
+        "당신은 세계 최고의 점성술사입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 챗도지입니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다.",
+    },
+    {
+      role: "assistant",
+      content:
+        "안녕하세요! 저는 챗도지입니다. 운세와 점성술에 관한 질문이 있으신가요? 어떤 것이든 물어보세요, 최선을 다해 답변해 드리겠습니다.",
+    },
+  ];
+
+  // 문자열을 깔끔하게 처리하는 과정이 필요하다
+  // 맨 앞에서 부터 꺼내기 위해 shift 사용한다
+  while (userMessages.length != 0 || assistantMessages.length != 0) {
+    if (userMessages.length != 0) {
+      messages.push(
+        JSON.parse(
+          '{"role": "user", "content": "' +
+            String(userMessages.shift()).replace(/\n/g, "") +
+            '"}'
+        )
+      );
+    }
+    if (assistantMessages.length != 0) {
+      messages.push(
+        JSON.parse(
+          '{"role": "assistant", "content": "' +
+            String(assistantMessages.shift()).replace(/\n/g, "") +
+            '"}'
+        )
+      );
+    }
+  }
+
+  // 지금까지 주고 받은 대화를 반영해서 응답을 보내준다
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content:
-          "당신은 세계 최고의 점성술사입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 챗도지입니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다.",
-      },
-      {
-        role: "user",
-        content:
-          "당신은 세계 최고의 점성술사입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 챗도지입니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다.",
-      },
-      {
-        role: "assistant",
-        content:
-          "안녕하세요! 저는 챗도지입니다. 운세와 점성술에 관한 질문이 있으신가요? 어떤 것이든 물어보세요, 최선을 다해 답변해 드리겠습니다.",
-      },
-      { role: "user", content: "오늘의 운세가 뭐야?" },
-    ],
+    messages: messages,
   });
   let fortune = completion.data.choices[0].message["content"];
-  console.log(fortune);
+
   res.json({ assistant: fortune });
 });
 
